dcb37398cde5506e1653108e1ae9a2f1ffd9eb19
beyfendipenguen <beyfendipenguen@gmail.com>
2021-11-10 14:49:50 +0300

    created quiz model

--- quizes/models.py
+++ quizes/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+DIFICULTLY_CHOICES = (
+    ('easy', 'easy'),
+    ('medium', 'medium'),
+    ('hard', 'hard')    
+)
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="duration of the quiz in minutes")
+    required_score_to_pass = models.IntegerField(help_text="required score in %")
+    difficultly = models.CharField(max_length=6,choices=DIFICULTLY_CHOICES)
+
+    class Meta:
+        verbose_name = _("Quiz")
+        verbose_name_plural = _("Quizes")
+
+    
+    def get_questions(self):
+        pass
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+
+    def get_absolute_url(self):
+        return reverse("Quiz_detail", kwargs={"pk": self.pk})
