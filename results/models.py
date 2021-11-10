71cfe8c8019390a61c08bc9b625fdf324c95978b
beyfendipenguen <beyfendipenguen@gmail.com>
2021-11-10 15:52:25 +0300

    created result model

--- results/models.py
+++ results/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from quizes.models  import Quiz
 # Create your models here.
+
+class Result(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.FloatField()
+
+
+
+    class Meta:
+        verbose_name = _("Result")
+        verbose_name_plural = _("Results")
+
+    def __str__(self):
+        return self.pk
+
+    def get_absolute_url(self):
+        return reverse("Result_detail", kwargs={"pk": self.pk})
