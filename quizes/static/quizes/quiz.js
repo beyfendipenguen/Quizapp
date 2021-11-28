812008be062b96f0910a9782540390898d02e418
beyfendipenguen <beyfendipenguen@gmail.com>
2021-11-28 16:11:12 +0300

    finished project

--- quizes/static/quizes/quiz.js
+++ quizes/static/quizes/quiz.js
@@ -4,7 +4,45 @@ const url = window.location.href
 const quizBox = document.getElementById("quiz-box")
 const scoreBox = document.getElementById("score-box")
 const resultBox = document.getElementById("result-box")
+const timerBox = document.getElementById("timer-box")
 
+const activateTimer = (time) => {
+
+  
+    let minutes = time - 1 
+    let seconds = 60
+    let displaySeconds
+    let displayMinutes  
+    
+    const timer = setInterval(()=>{
+        seconds --
+        if (seconds < 0) {
+            seconds = 59
+            minutes --
+
+        }
+        if (minutes.toString().length < 2) {
+            displayMinutes = "0"+minutes
+        }else{
+            displayMinutes = minutes
+        }
+        if(seconds.toString().length < 2){
+            displaySeconds = "0"+seconds
+        }else{
+            displaySeconds = seconds
+        }
+        if (minutes === 0 && seconds === 0) {
+            timerBox.innerHTML =`<b>00:00</b>`
+            setTimeout(()=>{
+                clearInterval(timer)
+                alert("Time Over")   
+                sendData()
+            },500)
+        }
+        timerBox.innerHTML = `<b>${displayMinutes}:${displaySeconds}</b>`
+
+    }, 1000)
+}
 
 $.ajax({
     type:'GET',
@@ -30,6 +68,7 @@ $.ajax({
                 })
             }
         })
+        activateTimer(response.time)
     },
     error: function(error){
         console.log(error)
